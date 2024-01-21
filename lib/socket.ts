601c92ef5f0b67fd148651896697cf23b5334072
bipinemp <bhandaribipin3.39@gmail.com>
2024-01-21 23:24:51 +0545

    changes

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,6 @@
 // socket.js
 import io from "socket.io-client";
 
-// const socket = io("wss://chatapp-xsy3.onrender.com/");
-const socket = io("ws://localhost:8000");
+const socket = io("wss://chatapp-xsy3.onrender.com/");
 
 export default socket;
